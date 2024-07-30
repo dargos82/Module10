a1f18e22ff464f60024f74d0a2e5ca7c143f5b8d
dargos82 <142857091+dargos82@users.noreply.github.com>
2024-07-30 13:28:25 -0400

    Update guessNumber.s

--- guessNumber.s
+++ guessNumber.s
@@ -2,15 +2,17 @@
 #Author: David Blossom
 #Date: 4 August 2024
 #Purpose: Guess a randomly generated number
-#Input: maximum - integer
-#Output: output - calculated prime numbers
+#Input: maximum - integer; guess - integer; control - integer
+#Output: prompt - get maximum value for range; guessPrompt - get the user's guess; correctGuess - output when number is guessed;
+#controlPrompt - get user input to continue or quit; endGame - output when game is ended; tooHigh - output when guessed number
+#is too high; tooLow - output when guessed number is too low
 #Ref:
 #Program dictionary:
 #	r4: generated number
 #	r5: maximum
 #	r6: guess
 #	r7: number of guesses
-#	r8: 
+#	r8: outer loop control
 #	r9: 
 
 
@@ -28,16 +30,16 @@ main:
     STR r8, [sp, #20]
     STR r9, [sp, #24]
 
-    #prompt user 
+    #prompt user for maximum value of range
     LDR r0, =prompt1
     BL printf
 
-    #read user input
+    #read user input			//scan and load maximum value of range
     LDR r0, =maximumFormat
     LDR r1, =maximum
     BL scanf
 
-    #move maxiumum to r5		//r5 has the upper bound
+    #move maxiumum to r5		//r5 has the maximum value
     LDR r5, =maximum
     LDR r5, [r5]
 
@@ -47,50 +49,78 @@ main:
     MOV r4, r0				//r4 = generated random number
     //MOV r4, #50			//temporary until figure out random number
 
-    #Initialize outer loop
+    MOV r8, #0				//initialize outer loop control to 0
     MOV r7, #0				//initialize number of guesses to 0
-
+    
     StartOuterLoop:
-	#check limit	
-	
-	#get the guess
-    	MOV r1, r5
-    	LDR r0, =guessPrompt
-    	BL printf
-	
-    	#store the guess
-    	LDR r0, =guessFormat
-    	LDR r1, =guess
-    	BL scanf
-	
-    	#move guess to r6		//r6 has the guess
-    	LDR r6, =guess
-    	LDR r6, [r6]
-	
-	ADD r7, r7, #1			//increment counter
-	CMP r6, r4			//check if guess is correct
-	BEQ EndOuterLoop		//if guess = generated number, exit loop
+    #Initialize outer loop
+    MOV r0, #2
+    CMP r8, r0
+    BEQ EndOuterLoop			//if player has not selected to quit, go to inner loop
+    	
+   	#loop statement
 
-	#loop statement
-
-	#Initialize inner loop
-	
 	StartInnerLoop:
-	    #check limit
-	    	    
+	    #Initialize inner loop
+	    	    	    
 	    #loop statement
+
+     	    #get the guess		//prompt for guess
+    	    MOV r1, r5
+    	    LDR r0, =guessPrompt
+    	    BL printf
+	
+    	    #store the guess		//scan and load the guess
+    	    LDR r0, =guessFormat
+    	    LDR r1, =guess
+    	    BL scanf
+	
+    	    #move guess to r6		//r6 = guess
+    	    LDR r6, =guess
+    	    LDR r6, [r6]
+	
+	    ADD r7, r7, #1		//increment counter
+     
+	    CMP r6, r4			//check if guess is correct
+	    BEQ EndInnerLoop		//if guess = generated number, exit loop
+
+     	    CMP r6, r4
+	    BGT tooHigh
+
+     	    CMP r6, r4
+	    BLT tooLow
 	    
-	    #get next value
+     	    tooHigh:
+	  	LDR r0, =tooHigh
+    		B StartInnerLoop
+
+      	    tooLow:
+	   	LDR r0, =tooLow
+     		B StartInnerLoop
 	
 	EndInnerLoop:	
 
-	#get next value
+	MOV r1, r7			//move number of guesses from r7 to r1 for printing
+	LDR r0, =correctGuess
+	BL printf
+	
+ 	#get next value			//prompt for user control input
+  	LDR r0, =controlPrompt
+   	BL printf
+
+        #store the guess		//scan and load user control input
+    	LDR r0, =controlFormat
+    	LDR r1, =control
+    	BL scanf
+
+ 	LDR r8, =control		//load r8 with user contrl input
+  	LDR r8, [r8]
+        B StartOuterLoop   	
 
     EndOuterLoop:
-	MOV r1, r7
-	LDR r0, =correctGuess
-	BL printf	
-	   
+    LDR r0, =endGame
+    BL printf
+  
     #pop the stack
     LDR lr, [sp, #0]
     LDR r4, [sp, #4]
@@ -104,7 +134,7 @@ main:
 
 .data
     #prompt for input
-    prompt1:	.asciz  "\nEnter a positive number: "
+    prompt1:	.asciz  "\nEnter a positive number for maximum value of guessing range: \n"
     
     #format for maximum input
     maximumFormat:	.asciz  "%d"
@@ -113,7 +143,7 @@ main:
     maximum:	.word	0
 
     #prompt for guess
-    guessPrompt:	.asciz	"Please enter a number between 0 and %d.\n"
+    guessPrompt:	.asciz	"Please enter a number between 1 and %d: \n"
 
     #format for guess
     guessFormat:	.asciz	"%d"
@@ -123,4 +153,22 @@ main:
 
     #output for correct guess
     correctGuess:	.asciz	"\nCongratulations!  You guessed the number in %d guess(es)!"
+
+    #output for game control
+    controlPrompt:	.asciz	"\nWould you like to play again? 1 to continue; 2 to quit: /n"
+
+    #control format
+    controlFormat:	.asciz	"%d"
+
+    #control value
+    control:	.word	0
+
+    #end statement
+    endGame:	.asciz	"/nThank you for playing!"
+
+    #too high statement
+    tooHigh:	.asciz	"/nToo high!  Try again./n"
+
+    #too low statement
+    tooLow:	.asciz	"/nToo low!  Try again./n"
 
